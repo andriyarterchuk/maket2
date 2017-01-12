30646f6798817d638034c806189138e36935e5d3
andriyarterchuk <andriyarterchuk@gmail.com>
2017-01-12 21:16:19 +0200

    added page v1.0.0

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
+    $("#form1").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 5,
+                maxlength: 30
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        }
+    });
+
+});
